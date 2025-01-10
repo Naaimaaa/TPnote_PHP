d1c36694e09dba309baf8767a56f39f51120de29
OphelieVal <ophelie.valin@etu.uni-orleans.fr>
2025-01-10 09:18:10 +0100

    Ajout insertions pour test + update MCD

--- DataBase/procedures.sql
+++ DataBase/procedures.sql
@@ -9,14 +9,14 @@ DROP TRIGGER IF EXISTS addReponseQuestion;
 
 -- Vérification qu'une question n'a pas de réponse en double
 delimiter |
-CREATE OR REPLACE TRIGGER addReponseQuestion BEFORE INSERT ON ASSOCIER FOR EACH ROW
+CREATE OR REPLACE TRIGGER addReponseQuestion BEFORE INSERT OR UPDATE ON ASSOCIER FOR EACH ROW
 BEGIN
     DECLARE nbReponseQ int DEFAULT 0;
-    DECLARE mes VARCHAR(100) DEFAULT "";
+    DECLARE mes VARCHAR(200) DEFAULT "";
     SELECT COUNT(NUMR) INTO nbReponseQ FROM ASSOCIER WHERE NUMR=new.NUMR;
     IF nbReponseQ = 1 THEN
-        SET mes = CONCAT("Impossible d'ajouter la réponse numéro", new.NUMR, "dans le questionnaire n°"
-        , new.NUMQ, "car cette réponse est déjà associée à celui-ci");
+        SET mes = CONCAT("Impossible d'ajouter la réponse n°", new.NUMR, " dans le questionnaire n°"
+        , new.NUMQ, " car cette réponse est déjà associée à celui-ci");
         signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
     END IF;
 END |
