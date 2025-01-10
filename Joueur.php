75041630d15227e4bb2a9585203b20852b8e793e
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-01-10 08:18:40 +0100

    codage classe joueur avec getters

---
+++ Joueur.php
@@ -0,0 +1,18 @@
+<?php
+class Joueur{
+    private $nom;
+    private $prenom;
+
+    public function __construct(String $nom, String $prenom){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function getPrenom(){
+        return $this->prenom;
+    }
+}
