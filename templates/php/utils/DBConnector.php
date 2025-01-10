934a403983c8816cd7ddd660c8ab4da7b7bf65cb
OphelieVal <ophelie.valin@etu.univ-orleans.fr>
2025-01-10 19:45:29 +0100

    Ajout de fonctions pour récupérer réponses et questions

--- templates/php/utils/DBConnector.php
+++ templates/php/utils/DBConnector.php
@@ -22,6 +22,45 @@ class DBConnector {
         return $rows;
     }
 
+   /**
+     * get_quiz, get l'ensemble des quiz de la BD
+     *
+     * @return array la liste des quiz
+     */
+    public function get_quiz():array{
+        $sql = "SELECT * FROM QUIZ;";
+        $stmt = $this->pdo->query($sql);
+        $rows = $stmt->fetchAll();
+        return $rows;
+    }
+
+    /**
+     * get_questions, get l'ensemble des questions d'un quiz de la BD
+     *
+     * @return array la liste des questions
+     */
+    public function get_questions_quiz(int $idQ):array{
+        $sql = "SELECT NUMQ, INTITULE FROM COMPOSER NATURAL JOIN QUESTION WHERE IDQUIZ= ?;";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$idQ]);
+        $questions = $stmt->fetchAll();
+        return $questions;
+    }
+
+    /**
+     * get_reponses, get l'ensemble des réponses à une question de la BD
+     *
+     * @return array la liste des réponses
+     */
+    public function get_reponses_question(int $numQ):array{
+        $sql = "SELECT NUMR, LIBELLE, CORRECTE FROM ASSOCIER NATURAL JOIN REPONSE WHERE NUMQ= ?;";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$numQ]);
+        $questions = $stmt->fetchAll();
+        return $questions;
+    }
+
+    
 
 }
 
