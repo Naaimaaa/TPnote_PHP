7068f0f293d6ea6eb5909033e12784f392e30003
OphelieVal <ophelie.valin@etu.univ-orleans.fr>
2025-01-11 08:48:56 +0100

    Modification arborescence

---
+++ templates/src/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require_once 'php/autoloader.php';
+Autoloader::register();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<?php 
+//include 'global/head.php'; 
+title_html("Let's Quiz");
+//link_to_css('static/accueil.css'); 
+?>
