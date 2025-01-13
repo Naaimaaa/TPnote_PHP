559efc6b7b71c3c8bc99e6d947099b4cfbc95202
OphelieVal <ophelie.valin@etu.uni-orleans.fr>
2025-01-13 15:16:51 +0100

    Ajout d'un head et header global pour les futures pages

---
+++ templates/global/header.php
@@ -0,0 +1,19 @@
+<style>
+
+</style>
+<header>
+    <div class="logo">
+        <a href="accueil.php">
+            <img src="#" alt="logo">
+        </a>
+       </div>
+    <nav>
+        <a href="#"> Nouveautés </a>
+        <a href="#"> Mes quiz </a>
+        <a href="#"> Contact </a>
+    </nav>
+    <div class="user-space">
+        <img src="#" alt="icone-user">
+        <a href="#"><img src="#" alt="icone-paramètres"></a>
+    </div>
+</header>
