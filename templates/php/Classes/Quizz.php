8f66c671cc186c5a7addf662f1b7085041fb9dcd
Naima <naimasanaakhtar@gmail.com>
2025-01-15 22:18:07 +0100

    modification classe Quizz.php

--- templates/php/Classes/Quizz.php
+++ templates/php/Classes/Quizz.php
@@ -1,20 +1,26 @@
 <?php
 namespace Classes;
 class Quizz{
+    private String $intitule;
     private $nbQuestion;
     private $score;
     private $lesQuestions;
     private $leJoueur;
 
-    public function __construct(Joueur $joueur){
+    public function __construct(String $intitule){
+        $this->intitule = $intitule;
         $this->nbQuestion = 5;
         $this->score = 0;
         $this->lesQuestions = array();
         $this->leJoueur = $joueur;
     }
 
+    public function getQuizz(){
+        return $this->intitule;
+    }
+
     public function getNbQuestions(){
-        return $this->getNbQuestions;
+        return $this->nbQuestion;
     }
 
     public function setNbQuestions(int $nb){
